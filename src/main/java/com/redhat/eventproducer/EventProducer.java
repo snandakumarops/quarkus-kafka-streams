8e9019998a9093678069228c7f3daa636ec5d1fc
Sadhana Nandakumar <snandaku@redhat.com>
2020-02-12 06:17:29 -0500

    UI func

--- src/main/java/com/redhat/eventproducer/EventProducer.java
+++ src/main/java/com/redhat/eventproducer/EventProducer.java
@@ -7,6 +7,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
@@ -22,6 +23,7 @@ public class EventProducer {
     @POST
     @Path("/txn-event/{custId}")
     @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public void postCase(String json,@javax.ws.rs.PathParam("custId") String customerId) {
 
         try {
